99e33f49d9f919020bb7baaf25b4e6b389a1d55b
Stanislavych <stasik.bartosh@mail.ru>
2024-04-30 08:30:21 +0300

    add entities

---
+++ src/Market.Domain/Entities/Category.cs
@@ -0,0 +1,8 @@
+﻿using Market.Domain.Common;
+
+namespace Market.Domain.Entities
+{
+    public class Category : BaseEntity
+    {
+    }
+}
---
+++ src/Market.Domain/Entities/Product.cs
@@ -0,0 +1,11 @@
+﻿using Market.Domain.Common;
+
+namespace Market.Domain.Entities
+{
+    public class Product : BaseEntity
+    {
+        public decimal Price { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
